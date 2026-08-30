a54625b2ed89ede4acfe481b856c6ac3ba40d1e9
Rajat Mondal <rajatrghs01@gmail.com>
2026-08-30 23:54:39 +0530

    Time: 0 ms (100%), Space: 16.4 MB (16.28%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,33 +1,45 @@
 class Solution {
 public:
-
-    int f(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp)
-    {
-        if(i == 0 && j == 0)
-        {
-            return grid[i][j];
-        }
-
-        if(i < 0 || j < 0)
-        {
-            return INT_MAX;
-        }
-
-        if(dp[i][j] != -1)
-        {
-            return dp[i][j];
-        }
-
-        int up = f(i - 1, j, grid, dp);
-        int left = f(i, j - 1, grid, dp);
-
-        return dp[i][j] = grid[i][j] + min(up, left);
-    }
-
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n,-1));
-        return f(m - 1, n - 1, grid, dp);
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp(n, vector<int>(m,0));
+
+        for(int i = 0;i< n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(i == 0 && j == 0)
+                {
+                    dp[i][j] = grid[i][j];
+                }
+                else
+                {
+                    int up = grid[i][j];
+                    if(i > 0)
+                    {
+                        up += dp[i-1][j];
+                    }
+                    else
+                    {
+                        up += 1e9;
+                    }
+
+                    int left = grid[i][j];
+                    if(j > 0)
+                    {
+                        left += dp[i][j-1];
+                    }
+                    else
+                    {
+                        left += 1e9;
+                    }
+
+                    dp[i][j] = min(left , up);
+                }
+            }
+        }
+
+        return dp[n-1][m-1];
     }
 };
